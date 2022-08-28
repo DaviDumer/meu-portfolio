bbbf7c4cff4da4dcea8e1d0af54e7af0fd1dd35f
Davi Guilherme <davidumer793@outlook.com>
2022-08-27 23:30:19 -0300

    27 08 Responsivo do header e animação do button hamburger

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import './css/main.css'; // Principal
 import './css/header.css';
+import './css/button-menu-hamburger.css';
 
-function component() {
-  const element = document.createElement('div');
-  element.classList.add('hello');
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = 'Hello Webpack';
-
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = netflix;
-  element.appendChild(myIcon);
-  return element;
+var buttonHamburger = document.querySelector('.js-header__button__menu');
+var headerLinksIn = document.querySelector('.js-header__links__in');
+var headerLinksSocial = document.querySelector('.js-header__links__social');
+var clickbuttonHamburger = false;
+function activeButtonHamburger(e) {
+  if (e.type === 'mouseover' && !clickbuttonHamburger) {
+    if (
+      e.target.classList.contains('js-header__button__menu') ||
+      e.target.parentElement.classList.contains('js-header__button__menu')
+    ) {
+      buttonHamburger.classList.add('header__button__menu--hover');
+    }
+  } else if (e.type === 'mouseout' && !clickbuttonHamburger) {
+    buttonHamburger.classList.remove('header__button__menu--hover');
+  }
+}
+function handleClickButtonHamburger(e) {
+  if (!clickbuttonHamburger) {
+    buttonHamburger.classList.add('header__button__menu--hover');
+    clickbuttonHamburger = true;
+  } else {
+    buttonHamburger.classList.remove('header__button__menu--hover');
+    clickbuttonHamburger = false;
+  }
 }
 
-// document.body.appendChild(component());
+buttonHamburger.onmouseover = activeButtonHamburger;
+buttonHamburger.onmouseout = activeButtonHamburger;
+buttonHamburger.onclick = handleClickButtonHamburger;
